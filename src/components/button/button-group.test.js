6b94910114e954e185d852ac9d62d551d263c985
chenxiaoyao <chenxiaoyao6228@163.com>
2020-04-18 15:31:38 +0800

    add test for Icon and ButtonGroup

---
+++ src/components/button/button-group.test.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/vue";
+import Button from "./button.vue";
+import ButtonGroup from "./button-group.vue";
+
+describe("ButtonGroup", () => {
+  let ButtonGroupTester = {
+    template: `
+        <ButtonGroup><Button icon="left">上一页</Button><Button icon="right" iconPosition="right">下一页</Button></ButtonGroup>`,
+    components: { ButtonGroup, Button },
+  };
+  test("validate child", () => {
+    const { container } = render(ButtonGroupTester, {});
+    expect(container.childNodes[0].childNodes.length).toBe(2);
+  });
+});
---
+++ src/components/icon/icon.test.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/vue";
+import Icon from "./icon.vue";
+
+describe("Icon", () => {
+  it("icon type", () => {
+    const { container } = render(Icon, {
+      propsData: {
+        type: "download",
+      },
+    });
+    expect(container.innerHTML).toBe(
+      '<svg class="g-icon"><use xlink:href="#icon-download" aria-hidden="true"></use></svg>'
+    );
+  });
+});
