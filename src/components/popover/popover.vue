a8319b9f3fbe44164b152344fb09dc9f40227b41
chenxiaoyao <chenxiaoyao6228@163.com>
2020-05-17 10:43:51 +0800

    fix popoverContent close issue

--- src/components/popover/popover.vue
+++ src/components/popover/popover.vue
@@ -4,9 +4,6 @@ import { getCoords } from '../../utils/dom';
 const prefixCls = 'g-popover';
 export default {
   name: 'Popover',
-  props: {
-
-  },
   data() {
     return {
       prefixCls,
@@ -22,23 +19,32 @@ export default {
       this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`;
       this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
     },
-    listenToDocument() {
-      const clickHandler = (event) => {
-        if (!this.$refs.contentWrapper.contains(event.target)) {
-          this.show = false;
-          document.removeEventListener('click', clickHandler);
-        }
-      };
-      document.addEventListener('click', clickHandler);
+    listenToDocument(event) {
+      const isPopoverWrapper = this.$refs.popover && this.$refs.popover === event.target;
+      const popoverContentWrapperElement = document.querySelector(`.${prefixCls}-content-wrapper`);
+      const isPopoverContent = event.target === popoverContentWrapperElement
+                              || popoverContentWrapperElement.contains(event.target);
+      if (isPopoverContent || isPopoverWrapper) return;
+      this.close();
+    },
+    open() {
+      this.show = true;
+      this.$nextTick(() => {
+        this.positionContent();
+        document.addEventListener('click', this.listenToDocument);
+      });
+    },
+    close() {
+      this.show = false;
+      document.removeEventListener('click', this.listenToDocument);
     },
     handleClick(e) {
       if (this.$refs.triggerWrapper.contains(e.target)) {
         this.show = !this.show;
         if (this.show) {
-          this.$nextTick(() => {
-            this.positionContent();
-            this.listenToDocument();
-          });
+          this.open();
+        } else {
+          this.close();
         }
       }
     }
@@ -48,6 +54,7 @@ export default {
 
 <template>
   <div
+    ref="popover"
     :class="[`${prefixCls}`]"
     @click.stop="handleClick"
   >
