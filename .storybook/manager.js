9613422d1adf1688e14ccbe0bf756cfb245f8482
chenxiaoyao <chenxiaoyao6228@163.com>
2021-02-06 00:03:37 +0800

    style: change storybook theming

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,67 +1,67 @@
 // manage.js: renders the UI of addons, the navigator and toolbar.
 import { addons } from "@storybook/addons";
-// import defaultTheme from './theme.default.js'
-
-addons.setConfig({
-  /**
-   * display the top-level grouping as a "root" in the sidebar
-   * @type {Boolean}
-   */
-  showRoots: false,
-  /**
-   * show story component as full screen
-   * @type {Boolean}
-   */
-  isFullscreen: false,
-
-  /**
-   * display panel that shows a list of stories
-   * @type {Boolean}
-   */
-  showNav: true,
-
-  /**
-   * display panel that shows addon configurations
-   * @type {Boolean}
-   */
-  showPanel: true,
-
-  /**
-   * where to show the addon panel
-   * @type {('bottom'|'right')}
-   */
-  panelPosition: "bottom",
-
-  /**
-   * sidebar tree animations
-   * @type {Boolean}
-   */
-  sidebarAnimations: true,
-
-  /**
-   * enable/disable shortcuts
-   * @type {Boolean}
-   */
-  enableShortcuts: true,
-
-  /**
-   * show/hide tool bar
-   * @type {Boolean}
-   */
-  isToolshown: true,
-
-  /**
-   * theme storybook, see link below
-   */
-  theme: undefined,
-
-  /**
-   * id to select an addon panel
-   * @type {String}
-   */
-  selectedPanel: undefined,
-});
+import defaultTheme from './theme.default.js'
 
 // addons.setConfig({
-//   theme: defaultTheme,
+//   /**
+//    * display the top-level grouping as a "root" in the sidebar
+//    * @type {Boolean}
+//    */
+//   showRoots: false,
+//   /**
+//    * show story component as full screen
+//    * @type {Boolean}
+//    */
+//   isFullscreen: false,
+
+//   /**
+//    * display panel that shows a list of stories
+//    * @type {Boolean}
+//    */
+//   showNav: true,
+
+//   /**
+//    * display panel that shows addon configurations
+//    * @type {Boolean}
+//    */
+//   showPanel: true,
+
+//   /**
+//    * where to show the addon panel
+//    * @type {('bottom'|'right')}
+//    */
+//   panelPosition: "bottom",
+
+//   /**
+//    * sidebar tree animations
+//    * @type {Boolean}
+//    */
+//   sidebarAnimations: true,
+
+//   /**
+//    * enable/disable shortcuts
+//    * @type {Boolean}
+//    */
+//   enableShortcuts: true,
+
+//   /**
+//    * show/hide tool bar
+//    * @type {Boolean}
+//    */
+//   isToolshown: true,
+
+//   /**
+//    * theme storybook, see link below
+//    */
+//   theme: undefined,
+
+//   /**
+//    * id to select an addon panel
+//    * @type {String}
+//    */
+//   selectedPanel: undefined,
 // });
+
+addons.setConfig({
+  theme: defaultTheme,
+});
--- .storybook/theme.default.js
+++ .storybook/theme.default.js
@@ -1,2 +1,12 @@
 // storybook mdx format docs is under development
 // https://github.com/storybookjs/storybook/blob/master/addons/docs/docs/theming.md
+import { create } from '@storybook/theming';
+
+import { version, name, repository } from "../package.json";
+
+export default create({
+  base: 'light',
+  brandTitle: `${name}-${version}`,
+  brandUrl: 'https://github.com/chenxiaoyao6228/graceful-ui',
+  //   brandImage: 'https://github.com/chenxiaoyao6228/cloudimg/blob/main/2021/logo-small.png?raw=true',
+});
