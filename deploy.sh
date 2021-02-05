701dfea40c522068ba67067eeeacb802cc43f4ea
chenxiaoyao <chenxiaoyao6228@163.com>
2021-02-05 23:19:42 +0800

    chore: update deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# throw error
+set -e
+
+# build static
+
+cd ./dist
+
+git config --global user.email "chenxiaoyao6228@163.com"
+git config --global user.name "chenxiaoyao6228"
+
+git init
+git add -A
+git commit -m 'deploy-to-github'
+
+git push -f https://${GITHUB_TOKEN}@github.com/chenxiaoyao6228/graceful-ui.git master:gh-pages
+
+cd -
+
+rm -rf ./dist
