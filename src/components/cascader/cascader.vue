ac4e3726dfb985595f38a35b51b88b1ba49ee827
chenxiaoyao <chenxiaoyao6228@163.com>
2021-02-07 18:10:22 +0800

    feat(cascader): 添加清除与下拉按钮

--- src/components/cascader/cascader.vue
+++ src/components/cascader/cascader.vue
@@ -1,10 +1,11 @@
 <script>
 import CascaderItem from './cascader-item.vue';
+import Icon from '../icon/icon.vue';
 
 export default {
   name: 'Cascader',
   components: {
-    CascaderItem
+    CascaderItem, Icon
   },
   props: {
     value: {
@@ -27,6 +28,10 @@ export default {
     },
     handlePopoverClose() {
       this.popoverVisible = false;
+    },
+    handleClear() {
+      this.handlePopoverClose();
+      this.$emit('input', []);
     }
   }
 };
@@ -39,22 +44,58 @@ export default {
   >
     <div
       class="g-cascader-trigger"
-      @click="popoverVisible = true"
+      :class="popoverVisible ?'active': ''"
+      @click="popoverVisible = !popoverVisible"
     >
-      {{ value }}
+      <div
+        class="g-cascader-select"
+      >
+        <div
+          v-if="value.length"
+          class="g-cascader-select-content"
+        >
+          {{ value.join('/') }}
+        </div>
+        <div
+          v-else
+          class="g-cascader-select-placeholder"
+        >
+          请选择
+        </div>
+        <div
+          v-show="(!popoverVisible && value.length)"
+          class="g-cascader-select-clear"
+          @click.stop="handleClear"
+        >
+          <Icon
+            type="error"
+          />
+        </div>
+        <div
+          v-show="popoverVisible && value.length"
+          class="g-cascader-select-caret"
+          :class="popoverVisible ? 'active':''"
+        >
+          <Icon
+            type="arrow-down"
+          />
+        </div>
+      </div>
     </div>
-    <div
-      v-if="popoverVisible"
-      class="g-cascader-popover"
-    >
-      <CascaderItem
-        :source="source"
-        :selected="value"
-        :level="0"
-        @close-popover="handlePopoverClose"
-        @update:selected="onUpdatedSelected"
-      />
-    </div>
+    <transition name="fade">
+      <div
+        v-if="popoverVisible"
+        class="g-cascader-popover"
+      >
+        <CascaderItem
+          :source="source"
+          :selected="value"
+          :level="0"
+          @close-popover="handlePopoverClose"
+          @update:selected="onUpdatedSelected"
+        />
+      </div>
+    </transition>
   </div>
 </template>
 
